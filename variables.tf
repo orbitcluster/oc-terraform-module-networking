e2f2e7dcb17fca32d5b251554abb6bc0eabe982f
maav-sk <shubhankarkhanda@gmail.com>
2025-12-22 15:19:06 +0530

    fix checkov error

--- variables.tf
+++ variables.tf
@@ -66,7 +66,7 @@ variable "env" {
   type        = string
 
   validation {
-    condition     = can(regex("^(dev|staging|prod)$", var.env))
+    condition     = can(regex("^(dev|staging|prod|test)$", var.env))
     error_message = "Environment must be dev, staging, or prod"
   }
 }
